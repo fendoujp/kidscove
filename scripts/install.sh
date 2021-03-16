a12f3d142f8c43b1782c4d6e21e2f7bfbf91e482
Xinguang <wangxinguang@iesolutions.jp>
2021-03-16 18:55:51 +0900

    change logo

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [[ ! -d node_modules ]]; then
-    docker run -it --rm --name kidscovebuild -v$(pwd):/work -w /work node:alpine sh -c '
+    docker run -it --rm --name kidscovebuild -v$(pwd):/work -w /work node:12-alpine sh -c '
         ls -la
         apk add --no-cache \
             alpine-sdk \
